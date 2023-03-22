617a4ace7c9975b71e73e5a59c053f0455ecac8f
Jian Zeng <anonymousknight96@gmail.com>
2023-03-22 20:56:18 +0800

    feat: print json of arbitrary object
    Signed-off-by: Jian Zeng <anonymousknight96@gmail.com>

--- diagnose.go
+++ diagnose.go
@@ -1,14 +1,42 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
+
 	"github.com/spf13/cobra"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/cli-runtime/pkg/resource"
+	_ "k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
 )
 
 func newDiagnoseCommand() *cobra.Command {
-	return &cobra.Command{
-		Use: "diagnose",
+	cf := genericclioptions.NewConfigFlags(true)
+	cmd := &cobra.Command{
+		Use:          "diagnose TYPE NAME",
+		SilenceUsage: true,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return nil
+			if len(args) != 2 {
+				return errors.New("must specify type and name")
+			}
+			ns, _, _ := cf.ToRawKubeConfigLoader().Namespace()
+			obj, err := resource.NewBuilder(cf).
+				NamespaceParam(ns).
+				DefaultNamespace().
+				WithScheme(scheme.Scheme, scheme.Scheme.PrioritizedVersionsAllGroups()...).
+				SingleResourceType().
+				ResourceNames(args[0], args[1]).Latest().Do().Object()
+			if err != nil {
+				return fmt.Errorf("get object: %w", err)
+			}
+			return json.NewEncoder(cmd.OutOrStdout()).Encode(obj)
 		},
 	}
+	flags := cmd.Flags()
+	flags.StringVar(cf.KubeConfig, "kubeconfig", *cf.KubeConfig, "Path to the kubeconfig file to use for CLI requests.")
+	flags.StringVarP(cf.Namespace, "namespace", "n", *cf.Namespace, "If present, the namespace scope for this CLI request")
+
+	return cmd
 }

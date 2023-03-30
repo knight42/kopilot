a9e1e29dc598bda480dcfe5c3f78cdda4816a3e2
Jian Zeng <anonymousknight96@gmail.com>
2023-03-30 22:00:17 +0800

    feat: prune objects more aggresively
    Signed-off-by: Jian Zeng <anonymousknight96@gmail.com>

--- cmd/diagnose/diagnose.go
+++ cmd/diagnose/diagnose.go
@@ -5,34 +5,34 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
-	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/yaml"
 
 	"github.com/knight42/kopilot/cmd"
 )
 
-func New(opt cmd.Options) *cobra.Command {
+func New(commonOpts cmd.Options) *cobra.Command {
 	c := &cobra.Command{
 		Use:          "diagnose TYPE NAME",
 		Short:        "Diagnose a resource",
 		SilenceUsage: true,
 		Args:         cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			ns, _, _ := opt.ToRawKubeConfigLoader().Namespace()
-			obj, err := opt.NewBuilder().
+			ns, _, _ := commonOpts.ToRawKubeConfigLoader().Namespace()
+			obj, err := commonOpts.NewBuilder().
 				NamespaceParam(ns).
 				DefaultNamespace().
-				Unstructured().
+				WithScheme(scheme.Scheme, scheme.Scheme.PrioritizedVersionsAllGroups()...).
 				ResourceNames(args[0], args[1]).
 				Do().
 				Object()
 			if err != nil {
 				return fmt.Errorf("get object: %w", err)
 			}
-			metaObj, err := meta.Accessor(obj)
-			if err == nil {
-				metaObj.SetManagedFields(nil)
-			}
+
+			pruneObject(obj)
+
+			// TODO: include logs or events
 			data, err := yaml.Marshal(obj)
 			if err != nil {
 				return fmt.Errorf("marshal object: %w", err)
@@ -41,13 +41,15 @@ func New(opt cmd.Options) *cobra.Command {
 			var buf bytes.Buffer
 			_ = promptDiagnose.Execute(&buf, templateData{
 				Data: string(data),
-				Lang: opt.Lang,
+				Lang: commonOpts.Lang,
 			})
 
-			return opt.NewChatGPTClient(" Diagnosing...").
+			// TODO: limit the length of the prompt?
+			return commonOpts.NewChatGPTClient(" Diagnosing...").
 				CreateCompletion(cmd.Context(), buf.String(), cmd.OutOrStdout())
 		},
 	}
-	opt.AddFlags(c.Flags())
+	flags := c.Flags()
+	commonOpts.AddFlags(flags)
 	return c
 }

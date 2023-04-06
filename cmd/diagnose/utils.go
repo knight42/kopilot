cab894537347eb6039629206315dd94857a505ba
Jian Zeng <anonymousknight96@gmail.com>
2023-04-06 20:00:19 +0800

    feat(diagnose): include related events (#16)
    Signed-off-by: Jian Zeng <anonymousknight96@gmail.com>

---
+++ cmd/diagnose/utils.go
@@ -0,0 +1,28 @@
+package diagnose
+
+import (
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/duration"
+)
+
+// translateMicroTimestampSince returns the elapsed time since timestamp in
+// human-readable approximation.
+func translateMicroTimestampSince(timestamp metav1.MicroTime) string {
+	if timestamp.IsZero() {
+		return "<unknown>"
+	}
+
+	return duration.HumanDuration(time.Since(timestamp.Time))
+}
+
+// translateTimestampSince returns the elapsed time since timestamp in
+// human-readable approximation.
+func translateTimestampSince(timestamp metav1.Time) string {
+	if timestamp.IsZero() {
+		return "<unknown>"
+	}
+
+	return duration.HumanDuration(time.Since(timestamp.Time))
+}
